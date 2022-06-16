fa48f082c8f16525b934d372844fee8b47cb2b85
Finja Thiemann <finja.thiemann@urs-os.de>
2022-06-16 14:20:45 +0200

    ...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Teams;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('teams') ->insert(['heim_id' => '', 'gast_id' => '' , 'group' => 'A' , 'tore_heim' => '0' , 'tore_gast' => '0' , 'spielzeit' => '' , 'spielort' => 'al-Thumama Stadium']);
+        DB::table('teams') ->insert(['heim_id' => '', 'gast_id' => '' , 'group' => 'B' , 'tore_heim' => '0' , 'tore_gast' => '0' , 'spielzeit' => '' , 'spielort' => 'Khalifa International Stadium']);
+        DB::table('teams') ->insert(['heim_id' => '', 'gast_id' => '' , 'group' => 'A' , 'tore_heim' => '0' , 'tore_gast' => '0' , 'spielzeit' => '' , 'spielort' => 'al-Bayt Stadium']);
+        DB::table('teams') ->insert(['heim_id' => '', 'gast_id' => '' , 'group' => 'B' , 'tore_heim' => '0' , 'tore_gast' => '0' , 'spielzeit' => '' , 'spielort' => 'Ahmed bin Ali Stadium']);
+        DB::table('teams') ->insert(['heim_id' => '', 'gast_id' => '' , 'group' => 'C' , 'tore_heim' => '0' , 'tore_gast' => '0' , 'spielzeit' => '' , 'spielort' => 'Lusail Stadium']);
+
     }
 }
